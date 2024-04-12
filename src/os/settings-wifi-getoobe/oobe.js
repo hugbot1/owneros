c27f8828c86af6da9a60670f6069a35d7540bc15
hugbot1 <132775034+hugbot1@users.noreply.github.com>
2024-04-12 17:29:23 -0500

    V 2.0.0

---
+++ src/os/settings-wifi-getoobe/oobe.js
@@ -0,0 +1,15 @@
+function checkupdate() {
+    window.open("./oobe/updatecheck.html", "_self")
+}
+function namethispc() {
+    window.open("user.html", "_self")
+}
+function nameofwho() {
+    window.open("whowillusethispc.html", "_self")
+}
+function setupdone() {
+    window.open("gettingeverything.html", "_self")
+}
+function done() {
+    window.open("../desktop-settings.html", "_self")
+}
